4250bf23e8d43055c769de07e603a3db23782934
GhCristea <g.cristea@studenti.uniba.it>
2018-06-17 16:42:50 +0200

    added class src/main/java/server/

---
+++ src/main/java/server/MultiServer.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author Cristea Gheorghita
+ *
+ */
+public class MultiServer {
+
+	static int PORT = 8080;
+	public MultiServer(int pORT2) {
+		
+	}
+	
+	void run() {
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MultiServer multiServer = new MultiServer(PORT);
+	}
+
+}
